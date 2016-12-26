5996476c47dec8c9aa39dc65c2e43f5dace9ea9c
Carl-Fredrik Herö <carlfredrik.hero@gmail.com>
2016-12-26 11:59:31 +0100

    Added forward function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Snake
 Trying to create the classic snake game
+
+To start in dev mode: `webpack-dev-server --content-base build/`
--- app/main.js
+++ app/main.js
@@ -18,10 +18,10 @@ const state = {
   add: 0,
   length: 1,
   turns: [
-    {x: 295, y: 295},
-    {x: 205, y: 295},
-    {x: 205, y: 205},
-    {x: 455, y: 205},
+    {x: 295, y: 295, color: 'white', dir: RIGHT},
+    {x: 205, y: 295, color: 'blue', dir: RIGHT},
+    {x: 205, y: 205, color: 'green', dir: DOWN},
+    {x: 215, y: 205, color: 'red', dir: LEFT},
   ]
 }
 
@@ -39,21 +39,20 @@ const draw = (el) => {
   state.turns.forEach((turn, index) => {
     let line = calcLine(index)
 
-    if (line) {
-      //console.log(index, line, turn)
-      console.log(turn.x,turn.y, line.x, line.y)
-      ctx.fillRect(turn.x,turn.y, line.x, line.y)
-    }
+    ctx.fillStyle = turn.color
+    ctx.fillRect(turn.x,turn.y, line.x, line.y)
   })
 }
 
 const calcLine = (i) => {
-  if (!i){
-    return false;
-  }
+  let x, y
 
-  let x = (state.turns[i-1].x - state.turns[i].x)
-  let y = (state.turns[i-1].y - state.turns[i].y)
+  if (i) {
+    x = (state.turns[i-1].x - state.turns[i].x)
+    y = (state.turns[i-1].y - state.turns[i].y)
+  } else {
+    x = y = BLOCK_SIZE
+  }
 
   return {
     x: (x) ? x : BLOCK_SIZE,
@@ -61,4 +60,52 @@ const calcLine = (i) => {
   }
 }
 
+const forward = () => {
+  let x, y
+  // 1. Move turn 0 1 BLOCK_SIZE in its direction
+  let head = state.turns[0]
+
+  switch (head.dir){
+    case RIGHT:
+      state.turns[0].x = state.turns[0].x + BLOCK_SIZE
+  }
+
+
+  // 2. Remove 1 BLOCK_SIZE from turn[length-1]
+  let tail = state.turns[state.turns.length-1]
+
+  switch (tail.dir){
+    case LEFT:
+      tail.x = tail.x - BLOCK_SIZE
+      break;
+    case DOWN:
+      tail.y = tail.y + BLOCK_SIZE
+  }
+
+  let next_tail = state.turns[state.turns.length-2]
+
+  // remove tail if it's at
+  console.log(`tail.x === next_tail.x && tail.y === next_tail.y`)
+  console.log(`${tail.x} === ${next_tail.x} && ${tail.y} === ${next_tail.y}`)
+  if (tail.x === next_tail.x && tail.y === next_tail.y) {
+    state.turns.splice(-1)
+    switch (next_tail.dir){
+    case DOWN:
+      next_tail.y = next_tail.y + BLOCK_SIZE
+    }
+  }
+}
+
 draw(document.querySelector('#gc'))
+console.log('state before: ', state)
+setTimeout(() => {
+  forward()
+  console.log('state after: ', state)
+  draw(document.querySelector('#gc'))
+  setTimeout(() => {
+    forward()
+    console.log('state after: ', state)
+    draw(document.querySelector('#gc'))
+
+  }, 5000)
+}, 5000)
